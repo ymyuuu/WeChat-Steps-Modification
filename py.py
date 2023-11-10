a40c7a54059c3ff3d4b5f3d2b0e7843c9a9b57e4
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2023-11-11 03:25:50 +0800

    Update py.py

--- py.py
+++ py.py
@@ -3,11 +3,8 @@ import requests
 import base64
 import random
 
-# 从 Secrets 中读取 Telegram API Token 和聊天 ID
 telegram_api_token = os.environ['TELEGRAM_API_TOKEN']
 telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
-
-# 从 Secrets 中读取账号和密码
 accounts_and_passwords = os.environ['ACCOUNTS_AND_PASSWORDS']
 account_password_pairs = [pair.split(',') for pair in accounts_and_passwords.split(';')]
 
@@ -16,49 +13,55 @@ def send_telegram_message(message):
     data = {
         'chat_id': telegram_chat_id,
         'text': message,
-        'parse_mode': 'HTML'  # 使用HTML格式
+        'parse_mode': 'HTML'
     }
     response = requests.post(telegram_url, data=data)
 
-def modify_steps(account, password, min_steps=None, max_steps=None):
-    try:
-        encoded_url = 'aHR0cDovL2JzLnN2di5pbmsvaW5kZXgucGhw'
-        url = base64.b64decode(encoded_url).decode('utf-8')
-        steps = random.randint(min_steps, max_steps)
-        data = {
-            'account': account,
-            'password': password,
-            'steps': steps
-        }
+def modify_steps(account, password, min_steps=None, max_steps=None, attempts=3):
+    consecutive_failures = 0
+    for _ in range(attempts):
+        try:
+            encoded_url = 'aHR0cDovL2JzLnN2di5pbmsvaW5kZXgucGhw'
+            url = base64.b64decode(encoded_url).decode('utf-8')
+            steps = random.randint(min_steps, max_steps)
+            data = {
+                'account': account,
+                'password': password,
+                'steps': steps
+            }
 
-        response = requests.post(url, data=data)
-        result = response.json()
+            response = requests.post(url, data=data)
+            result = response.json()
 
-        # 检查响应是否不是"success"，然后发送Telegram消息
-        if result.get('message') != 'success':
-            telegram_message = f"<b>Steps_modifier</b>\n\n账号： {account}\n响应： {result.get('message', 'No message found in response')}"
+            if result.get('message') == 'success':
+                return {
+                    'account': account,
+                    'response': result.get('message', 'No message found in response')
+                }
+            else:
+                consecutive_failures += 1
+        except Exception as e:
+            consecutive_failures += 1
+
+        if consecutive_failures == 3:
+            telegram_message = f"<b>Steps_modifier</b>\n\n账号： {account}\n连续三次失败"
             send_telegram_message(telegram_message)
+            return {
+                'account': account,
+                'response': "Exceeded maximum consecutive failures"
+            }
 
-        return {
-            'account': account,
-            'response': result.get('message', 'No message found in response')
-        }
-    except Exception as e:
-        telegram_message = f"<b>Steps_modifier</b>\n\n账号： {account}\n错误： {str(e)}"
-        send_telegram_message(telegram_message)
-        return {
-            'account': account,
-            'response': str(e)
-        }
+    return {
+        'account': account,
+        'response': "Exceeded maximum attempts"
+    }
 
-# 默认使用了最小步数为50000，最大步数为80000
 if __name__ == "__main__":
     min_steps = 50000
     max_steps = 80000
 
     for account, password in account_password_pairs:
         result = modify_steps(account, password, min_steps, max_steps)
-        # 隐藏账号的中间部分
         hidden_account = account[:3] + '*' * (len(account) - 6) + account[-3:]
 
         print("账号:", hidden_account)
